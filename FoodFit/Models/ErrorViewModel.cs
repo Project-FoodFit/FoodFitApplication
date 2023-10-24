d01ee2078bee352cd8f7740b277f4d0fd9fb8756
detsu4hxE <rybalkomihail744@gmail.com>
2023-10-24 21:51:11 +0300

    Добавьте файлы проекта.

---
+++ FoodFit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FoodFit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
